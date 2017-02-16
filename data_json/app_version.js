b4465497571e25cb153d0bfe4f2fdf51a8ad205e
Jeby Sun <jebysun@126.com>
2017-02-16 17:26:43 +0800

    添加json数据描述文件和readme.

---
+++ data_json/app_version.js
@@ -0,0 +1,9 @@
+{
+	"versionCode":10,
+	"versionName":"1.0.0",
+	"fileSize":"6.2M",
+	"apkUrl":"http://58.216.107.44/imtt.dd.qq.com/16891/36C5694F6FE468D788FFFC65166547BE.apk?mkey=58a403869c7c4c41&f=858&c=0&fsname=com.qiyi.video_8.1_80830.apk&csr=4d5s&p=.apk",
+	"required":false,
+	"publishDate":"2017-02-12 12:45:20",
+	"updateMsgList":["新版本特性新版本特性新版本特性。", "描述版本信息，方便用户选择是否下载便用户选择是否下载便。", "性能优化和BUG修复。"]
+}
